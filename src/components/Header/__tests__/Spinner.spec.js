9769b25e5d6aa246e69c9a73464fc7335a0ffc5b
Cristian Muscalu <muscaiu@yahoo.com>
2019-01-16 14:12:10 +0200

    add coverage report with junit

--- src/components/Header/__tests__/Spinner.spec.js
+++ src/components/Header/__tests__/Spinner.spec.js
@@ -6,26 +6,7 @@ import Spinner from '../Header';
 
 describe('Header', () => {
     it('should match snapshot', () => {
-        // const wrapper = shallow(<Spinner />, { context: { store } });
         const wrapper = shallow(<Spinner />);
         expect(toJson(wrapper)).toMatchSnapshot();
     })
-    it('check if it contains a div', () => {
-        const wrapper = shallow(<Spinner />);
-        expect(wrapper.find('div').hasClass('preloader')).to.equal(true);
-    });
-
-
-    // it('Comment should be rendered correctly', () => {
-    //     // Create the component
-    //     const CommentComponent = shallow(<Comment store={mockStore} {...sampleProps} />
-    //     );
-
-    //     const commentHeaderComponent = CommentComponent.find('CommentHeader').length;
-    //     const CommentBodyComponent = CommentComponent.find('CommentBody');
-
-    //     expect(CommentComponent.length).toEqual(1);
-    //     expect(commentHeaderComponent.length).toEqual(1);
-    //     expect(CommentBodyComponent.length).toEqual(1);
-    // });
 })
